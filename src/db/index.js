f0906841e8b216e2b9d8162b72966be8185e6280
Sandipbgn <iamsbgn@gmail.com>
2025-05-16 08:39:41 +0545

    commit by sandip

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try{ 
-        const connectionInstance =await mongoose.connect(`/${process.env.MONGODB_URL}/${DB_NAME}`, )
-        console.log(`\n MongoDB connected successfullyn ${connectionInstance.connection.host} `);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected successfully: ${connectionInstance.connection.host} `);
 
     }catch(error){
         console.log("Error connecting to MongoDB", error);
